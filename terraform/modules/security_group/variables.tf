19e81026e15233dbcc48fa7dc13c0b4bf102378a
DIEGO LEWENSZTAIN ZELAYA <diego.lewensztain@ucb.edu.bo>
2026-05-20 19:37:53 -0400

    terraform: agregar variables del módulo security_group

---
+++ terraform/modules/security_group/variables.tf
@@ -0,0 +1,9 @@
+variable "app_port" {
+  description = "Puerto de la aplicación"
+  type        = number
+}
+
+variable "my_ip" {
+  description = "IP permitida para acceso"
+  type        = string
+}
