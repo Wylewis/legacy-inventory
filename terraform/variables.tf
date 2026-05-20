92ab7d5671e55730618d64ba01bc133771682210
DIEGO LEWENSZTAIN ZELAYA <diego.lewensztain@ucb.edu.bo>
2026-05-20 19:33:48 -0400

    terraform: agregar variables

---
+++ terraform/variables.tf
@@ -0,0 +1,23 @@
+variable "aws_region" {
+  description = "Región de AWS"
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "instance_type" {
+  description = "Tipo de instancia EC2"
+  type        = string
+  default     = "t2.micro"
+}
+
+variable "app_port" {
+  description = "Puerto de la aplicación"
+  type        = number
+  default     = 3000
+}
+
+variable "my_ip" {
+  description = "Tu IP para acceso restringido"
+  type        = string
+  default     = "0.0.0.0/0"
+}
